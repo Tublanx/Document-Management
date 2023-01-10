89449720c4f3c224eba03e7284f72588cee211cb
Tublanx <rlgus03453@naver.com>
2023-01-10 22:27:14 +0900

    initialize project

---
+++ src/main/java/com/khfc/documentmng/DocumentmngApplication.java
@@ -0,0 +1,13 @@
+package com.khfc.documentmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentmngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentmngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khfc/documentmng/DocumentmngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khfc.documentmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentmngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
