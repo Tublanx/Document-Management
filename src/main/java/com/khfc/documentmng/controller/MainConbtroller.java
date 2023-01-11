42eacb153ae9a8685661bc934c8338653d3d5c77
Tublanx <rlgus03453@naver.com>
2023-01-11 20:42:50 +0900

    메인화면에 사용할 controller 추가

--- src/main/java/com/khfc/documentmng/controller/MainConbtroller.java
+++ src/main/java/com/khfc/documentmng/controller/MainConbtroller.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class MainConbtroller {
 
 	@GetMapping(value = "/main")
-	public String t() {
-		return "test";
+	public String main() {
+		return "main";
 	}
 	
 }
