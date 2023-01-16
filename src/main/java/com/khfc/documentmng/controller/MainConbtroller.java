244a4ba8d1b108c9a8eb7cc32225b3d61436342e
Tublanx <rlgus03453@naver.com>
2023-01-16 20:00:07 +0900

    HttpServletRequest로 유효한 세션을 가져온 후 model에 값 넣기

--- src/main/java/com/khfc/documentmng/controller/MainConbtroller.java
+++ src/main/java/com/khfc/documentmng/controller/MainConbtroller.java
@@ -1,14 +1,33 @@
 package com.khfc.documentmng.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainConbtroller {
 
 	@GetMapping(value = "/main")
-	public String main() {
+	public String main(HttpServletRequest request, Model model) {
+
+		HttpSession session = request.getSession();
+
+		if (session == null) {
+			return "login";
+		}
+
+		String userId = String.valueOf(session.getAttribute("userId"));
+
+		if (userId == null) {
+			return "login";
+		}
+
+		model.addAttribute("userId", userId);
+
 		return "main";
 	}
-	
+
 }
