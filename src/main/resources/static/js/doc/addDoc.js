0af5b1e655ed3107b215419ee91304b533c3755e
Tublanx <rlgus03453@naver.com>
2023-01-29 23:57:19 +0900

    엑셀 다운로드 로직 추가

---
+++ src/main/resources/static/js/doc/addDoc.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#exceldownload').click(function() {
+		const link = document.createElement('a');
+		const id = document.getElementById('id').value;
+		const today = new Date();
+
+		let year = today.getFullYear(); // 년도
+		let month = today.getMonth() + 1;  // 월
+		let date = today.getDate();  // 날짜
+
+		link.download = year + month + date + id + ".xls";
+		link.href = 'data:, ' + $('#exceldownload').text();
+		link.click();
+	});
+});
